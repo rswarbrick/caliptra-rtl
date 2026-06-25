b40ff53c09d3c7c528e77d1cc7bd6f5cd0678739
Rupert Swarbrick <rswarbrick@lowrisc.org>
2026-06-25 19:23:01 +0100

    [sha3_ctrl,dv] Copy kmac_test from OpenTitan as sha3_ctrl_test
    As with the environment, this gets renamed from "kmac" to "sha3_ctrl".
    Renaming in the test files is easier though: they only refer to the
    environment, which has been renamed.
    
    The only real change to the code is to connect up an AHB agent from
    the environment to the virtual sequences that we run (because
    sha3_ctrl_base_vseq wants to run AHB sequences directly, as well as
    through the register layer).

---
+++ src/sha3/dv/tests/sha3_ctrl_test_pkg.sv
@@ -0,0 +1,15 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+package sha3_ctrl_test_pkg;
+  `include "uvm_macros.svh"
+  `include "dv_macros.svh"
+
+  import uvm_pkg::*;
+  import sha3_ctrl_env_pkg::*;
+
+  import dv_lib_pkg::dv_base_test;
+
+  `include "sha3_ctrl_base_test.sv"
+endpackage
