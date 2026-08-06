060f227380c0ca02de6c201dc3eb0de3b791d317
Rupert Swarbrick <rswarbrick@gmail.com>
2026-08-06 08:48:51 +0100

    [dv] Teach dv_base_mem to have an extra layer of access restriction
    This slightly complicated dance is needed to represent the MSG_FIFO
    window in sha3_ctrl, which is write-only. Unfortunately, uvm_mem
    doesn't actually support those access permissions so we have to add
    this as an extra layer.

--- tools/dv-classes/dv_base_reg/dv_base_mem.sv
+++ tools/dv-classes/dv_base_reg/dv_base_mem.sv
@@ -18,6 +18,11 @@ class dv_base_mem extends uvm_mem;
   // If true, mem stores integrity along with data but it won't check the data integrity
   local bit data_intg_passthru;
 
+  // An extra access constraint set that applies as well as m_access. This can be set with
+  // set_access() and exists to allow us to model write-only memories, which are not supported
+  // directly by uvm_mem.
+  local string m_access_constraint = "RW";
+
   // Create a new instance of the memory abstraction class.
   //
   // The only access types supported are RW, RO and WO.
@@ -26,6 +31,21 @@ class dv_base_mem extends uvm_mem;
                       int unsigned     n_bits,
                       string           access = "RW",
                       int              has_coverage = UVM_NO_COVERAGE);
+
+  // Add an extra constraint to the access for this memory. This only supports "RW", "RO" and "WO"
+  // and should be considered as an extra layer on top of the access defined by the underlying
+  // uvm_mem.
+  //
+  // This is defined to allow an environment to constrain access to a memory after it has been
+  // created (by auto-generated code). The underlying uvm_mem only supports access values of RW and
+  // RO.
+  extern function void set_access(string access);
+
+  // Get the access for this memory through the given map (defaulting to the default map)
+  //
+  // This extends the implementation in uvm_mem, adding a possible extra access restriction from
+  // m_access_constraint.
+  extern function string get_access(uvm_reg_map map = null);
 
   extern function void set_mem_partial_write_support(bit enable);
   extern function bit get_mem_partial_write_support();
@@ -48,6 +68,51 @@ function dv_base_mem::new(string           name,
   super.new(name, size, n_bits, access, has_coverage);
   if (!(access inside {"RW", "RO", "WO"}))
     `uvm_error(`gfn, $sformatf("Memory can only be RW, RO or WO (saw %s)", access))
+endfunction
+
+function void dv_base_mem::set_access(string access);
+  if (! (access inside {"RW", "RO", "WO"})) begin
+    `uvm_fatal("bad_access",
+               $sformatf({"Cannot set access to '%0s': ",
+                          "the only supported values are RW, RO and WO."},
+                         access))
+  end
+  m_access_constraint = access;
+endfunction
+
+function string dv_base_mem::get_access(uvm_reg_map map = null);
+  bit    can_read = 1;
+  bit    can_write = 1;
+  string from_base = super.get_access(map);
+  string resolved;
+
+  case (from_base)
+    "RW": begin end
+    "RO": can_write = 0;
+    "WO": can_read = 0;
+    default: `uvm_fatal("bad_access",
+                        $sformatf("Base class returned unknown access value of %0s", from_base))
+  endcase
+
+  case (m_access_constraint)
+    "RW": begin end
+    "RO": can_write = 0;
+    "WO": can_read = 0;
+    default: `uvm_fatal("bad_access",
+                        $sformatf("Invalid m_access_constraint: %0s", m_access_constraint))
+  endcase
+
+  case ({can_read, can_write})
+    2'b01: return "WO";
+    2'b10: return "RO";
+    2'b11: return "RW";
+    default:
+      `uvm_fatal("bad_access",
+                 $sformatf({"Memory has been confgured to be impossible to access. ",
+                            "The uvm_mem and map together give access = %0s and ",
+                            "m_access_constraint = %0s."},
+                           from_base, m_access_constraint))
+  endcase
 endfunction
 
 function void dv_base_mem::set_mem_partial_write_support(bit enable);
