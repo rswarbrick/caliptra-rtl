1f4a049db4e758ca9ce486311eaebaa3eddd04a1
Rupert Swarbrick <rswarbrick@lowrisc.org>
2026-08-07 14:10:26 +0100

    [sha3_ctrl,dv] Define sha3_ctrl_shadow_reg_errors_test

--- src/sha3/dv/tests/sha3_ctrl_test_pkg.sv
+++ src/sha3/dv/tests/sha3_ctrl_test_pkg.sv
@@ -11,9 +11,12 @@ package sha3_ctrl_test_pkg;
 
   import dv_lib_pkg::dv_base_test;
 
+  import shadow_reg_errors_vseq_pkg::shadow_reg_errors_vseq;
+
   `include "sha3_ctrl_base_test.sv"
   `include "sha3_ctrl_intr_test_test.svh"
   `include "sha3_ctrl_same_csr_outstanding_test.svh"
   `include "sha3_ctrl_rand_reset_test.svh"
   `include "sha3_ctrl_csr_mem_rw_with_rand_reset_test.svh"
+  `include "sha3_ctrl_shadow_reg_errors_test.svh"
 endpackage
